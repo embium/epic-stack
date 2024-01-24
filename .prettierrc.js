af4ffa21d34f2f269b2e2de544bce9d74a42893c
Michael Mooney <mikeymooney1991@gmail.com>
2024-01-24 17:19:48 -0600

    switch to postgres for db

--- .prettierrc.js
+++ .prettierrc.js
@@ -17,7 +17,6 @@ export default {
 	singleQuote: true,
 	tabWidth: 2,
 	trailingComma: 'all',
-	useTabs: true,
 	overrides: [
 		{
 			files: ['**/*.json'],
--- tests/setup/db-setup.ts
+++ tests/setup/db-setup.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import fsExtra from 'fs-extra'
-import { afterAll, afterEach, beforeAll } from 'vitest'
-import { cleanupDb } from '#tests/db-utils.ts'
+import { /*afterAll, afterEach,*/ beforeAll } from 'vitest'
+//import { cleanupDb } from '#tests/db-utils.ts'
 import { BASE_DATABASE_PATH } from './global-setup.ts'
 
 const databaseFile = `./tests/prisma/data.${process.env.VITEST_POOL_ID || 0}.db`
@@ -9,11 +9,12 @@ const databasePath = path.join(process.cwd(), databaseFile)
 process.env.DATABASE_URL = `file:${databasePath}`
 
 beforeAll(async () => {
-	await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
+  await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
 })
 
 // we *must* use dynamic imports here so the process.env.DATABASE_URL is set
 // before prisma is imported and initialized
+/*
 afterEach(async () => {
 	const { prisma } = await import('#app/utils/db.server.ts')
 	await cleanupDb(prisma)
@@ -24,3 +25,4 @@ afterAll(async () => {
 	await prisma.$disconnect()
 	await fsExtra.remove(databasePath)
 })
+*/
